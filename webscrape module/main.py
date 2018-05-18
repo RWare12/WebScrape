7999e13997599395fc0bad85415903bc07efe90c
RWare12 <39044631+RWare12@users.noreply.github.com>
2018-05-18 14:13:33 +0800

    Add files via upload

--- webscrape module/main.py
+++ webscrape module/main.py
@@ -1,9 +1,15 @@
 import stackParser
 import saveJsonFile
 
-URL = input('ENTER URL: ') #getting URL
-if "https://stackoverflow.com/questions/" in URL:
-	result = stackParser.parse(URL)
-	saveJsonFile.saveFile(result)
-else:
-	print("Invalid URL!")
+URL = ''
+
+while(URL is not 'q'):
+    print('Enter \'q\' to exit!')
+    URL = input('ENTER URL: ') #getting URL
+    if "https://stackoverflow.com/questions/" in URL:
+            result = stackParser.parse(URL)
+            saveJsonFile.saveFile(result)
+    else:
+            print("Invalid URL!")
+
+exit()
